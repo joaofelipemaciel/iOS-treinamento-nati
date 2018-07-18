c7c721bdc185aa5b7bb6d34e16d020e35f9450fc
administrador <administrador@Mac-mini-de-administrador-3.local>
2018-07-18 12:15:28 -0300

    Agenda Telefonica - treinamento NATI

---
+++ DelegateExample/DelegateExample/MatriculaCounter.swift
@@ -0,0 +1,21 @@
+//
+//  MatriculaCounter.swift
+//  DelegateExample
+//
+//  Created by administrador on 17/07/2018.
+//  Copyright © 2018 administrador. All rights reserved.
+//
+
+import UIKit
+
+class MatriculaCounter: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NetworkExample/Pods/Target Support Files/Pods-NetworkExample/Pods-NetworkExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NetworkExample : NSObject
+@end
+@implementation PodsDummy_Pods_NetworkExample
+@end
